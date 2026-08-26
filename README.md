8df2e31db4c92169be12ad6c8bcccd5289fa3b7c
roshananair <roshanandres@gmail.com>
2026-08-26 11:41:20 -0700

    resolve changes

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hurricane effects on stream chemistry
+Recreation of Figure 3 from: Schaefer Douglas A, McDowell WH, Scatena FN, Asbury CE. Effects of hurricane disturbance on stream water concentrations and fluxes in eight tropical forest watersheds of the Luquillo Experimental Forest, Puerto Rico. Journal of Tropical Ecology. 2000;16(2):189-207. doi:10.1017/S0266467400001358.
+
+# 1) Data
+Luquillo Experimental Forest (LEF) Long Term Ecological Research (LTER)
+
+Data Access: McDowell, W. and International Institute of Tropical Forestry(IITF), USDA Forest Service.. 2024. Chemistry of stream water from the Luquillo Mountains ver 4923064. Environmental Data Initiative. https://doi.org/10.6073/pasta/f31349bebdc304f758718f4798d25458. 
+
+Download: Download and unzip the Full Data Package (Zip) [[knb-lter-luq.20.4923064](data/knb-lter-luq.20.4923064)] OR Download the following four packages—[14. Puente Roto Mameyes (MPR) Chemistry Data](data/knb-lter-luq.20.4923064/RioMameyesPuenteRoto.csv), [2. Quebrada one-Bisley (Q1) Chemistry Data](data/knb-lter-luq.20.4923064/QuebradaCuenca1-Bisley.csv), [3. Quebrada two-Bisley (Q2) Chemistry Data](data/knb-lter-luq.20.4923064/QuebradaCuenca2-Bisley.csv), and [4. Quebrada three-Bisley (Q3) Chemistry Data](data/knb-lter-luq.20.4923064/QuebradaCuenca3-Bisley.csv).
+
+Variables Used: Sample_ID (Site Name), Sample_Date (YYYY-MM-DD), K (mg/l), NO3-N (ug/l), Mg (mg/l), Ca (mg/l), NH4-N (ug/l).
+
+# 2) Scripts
+[Current in progress R script](scratch/scratch.R)
+
+# 3) Authors
+Roshan Andres Nair
+
+# 4) References
+1. McDowell, W. and International Institute of Tropical Forestry(IITF), USDA Forest Service.. 2024. Chemistry of stream water from the Luquillo Mountains ver 4923064. Environmental Data Initiative. https://doi.org/10.6073/pasta/f31349bebdc304f758718f4798d25458. 
+2. Schaefer Douglas A, McDowell WH, Scatena FN, Asbury CE. Effects of hurricane disturbance on stream water concentrations and fluxes in eight tropical forest watersheds of the Luquillo Experimental Forest, Puerto Rico. Journal of Tropical Ecology. 2000;16(2):189-207. doi:10.1017/S0266467400001358.
--- scratch/scratch.R
+++ scratch/scratch.R
@@ -9,31 +9,70 @@ bq3_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca3-Bisley.csv")
 
 # Pulling out columns for figure -----------------------------------------
 prm <- prm_data |>
-  select("Sample_ID", "Sample_Date", "NO3-N", "Mg", "Ca", "NH4-N") |>
+  select("Sample_ID", "Sample_Date", "NO3-N", "Mg", "Ca", "NH4-N", "K") |>
   glimpse()
 bq1 <- bq1_data |>
-  select("Sample_ID", "Sample_Date", "NO3-N", "Mg", "Ca", "NH4-N") |>
+  select("Sample_ID", "Sample_Date", "NO3-N", "Mg", "Ca", "NH4-N", "K") |>
   glimpse()
 bq2 <- bq2_data |>
-  select("Sample_ID", "Sample_Date", "NO3-N", "Mg", "Ca", "NH4-N") |>
+  select("Sample_ID", "Sample_Date", "NO3-N", "Mg", "Ca", "NH4-N", "K") |>
   glimpse()
 bq3 <- bq3_data |>
-  select("Sample_ID", "Sample_Date", "NO3-N", "Mg", "Ca", "NH4-N") |>
+  select("Sample_ID", "Sample_Date", "NO3-N", "Mg", "Ca", "NH4-N", "K") |>
   glimpse()
 
 
 # Combining 4 subsetted dataframes ---------------------------------------
 
-data <- combined <- rbind(prm, bq1, bq2, bq3) |>
-  pivot_longer(3:6) |>
-  glimpse() |>
-  rename(chemical = name) |>
-  glimpse()
+#combined <- rbind(prm, bq1, bq2, bq3) |>
+#glimpse()
+
+means <- tibble(
+  n_windows = c(1:41),
+  k_mean = NA,
+  mg_mean = NA,
+  ca_mean = NA,
+  nh4_mean = NA,
+  no3_mean = NA
+) |>
+  mutate("start" = seq(ymd("1988-01-01"), ymd("1994-12-31"), by = "9 weeks"))
+
+
+for (i in 1:nrow(means)) {
+  w1 <- means$start[i]
+  w2 <- w1 + 63
+  k <- prm$K[prm$Sample_Date >= w1 & prm$Sample_Date < w2]
+  mg <- prm$Mg[
+    prm$Sample_Date >= w1 & prm$Sample_Date < w2
+  ]
+  ca <- prm$Ca[
+    prm$Sample_Date >= w1 & prm$Sample_Date < w2
+  ]
+  nh4 <- prm$`NH4-N`[
+    prm$Sample_Date >= w1 & prm$Sample_Date < w2
+  ]
+  no3 <- prm$`NO3-N`[
+    prm$Sample_Date >= w1 & prm$Sample_Date < w2
+  ]
+  means$k_mean[i] <- mean(k, na.rm = TRUE)
+  means$mg_mean[i] <- mean(mg, na.rm = TRUE)
+  means$ca_mean[i] <- mean(ca, na.rm = TRUE)
+  means$nh4_mean[i] <- mean(nh4, na.rm = TRUE)
+  means$no3_mean[i] <- mean(no3, na.rm = TRUE)
+}
+
+prm_longer <-
+  pivot_longer(
+    means,
+    cols = 2:6,
+    names_to = "chemical",
+    values_to = "concentration"
+  )
 
 # Plotting ---------------------------------------------------------------
-data |>
+prm_longer |>
   ggplot(
-    mapping = aes(x = Sample_Date, y = value)
+    mapping = aes(x = start, y = concentration, color = chemical)
   ) +
   geom_line() +
   theme_bw() +
